fa24cf960d394cb3ded5fc2c9957fb7ab962bf78
kuronekomichael <hardwarehacking@gmail.com>
2017-02-09 22:01:17 +0900

    Fix: WebサイトのURLとタイトルをコピペするポップアップから、無駄な文言を削除するよう修正

--- popup.js
+++ popup.js
@@ -1,6 +1,18 @@
 /* global chrome */
 'use strict';
-
+/*
+ * 現在開いているサイトのタイトルとURLをコピペするためのポップアップ
+ */
 chrome.tabs.getSelected(null, function(tab) {
-    document.getElementById('status').innerText = tab.title + "\n" + tab.url;
+
+	// タイトル末尾の邪魔な文言を削除して、タイトルを取得
+	const title = [
+		/\s*-\s*\S+\s*-\s*Redmine$/,	// - Redmine
+		/\s*-\s*\S+\.esa\.io$/,		// - esa.io
+	].reduce(function(title, regex) {
+		return title.replace(regex, '');
+	}, tab.title);
+
+	// ポップアップ内に、タイトルとURLを表示する
+	document.getElementById('status').innerText = title + "\n" + tab.url;
 });
